e46839f1573f1eef7b6ed06393a176898f3d914e
Hedra Emad <emadhedra4@gmail.com>
2026-01-23 16:13:50 +0200

    Add bootstrap fills

---
+++ hedra/main.js
@@ -0,0 +1,34 @@
+async function getMeal(mealID) {
+  let meal = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`,
+  );
+
+  meal = await meal.json();
+  console.log(meal.meals[0]);
+}
+
+function displayMeal(meal) {
+  let html = ` <div class="col-md-4 myM text-white">
+        <img class="w-100" src="${meal.strMealThumb}" alt="" srcset=""><br>
+        <h1>${meal.strMeal}</h1>
+    </div>
+    <div class="col-md-8 myM text-white text-left">
+        <h2>Instructions</h2>
+        <p>${meal.strInstructions}</p>
+        <p><b class="fw-bolder">Area :</b> ${meal.strArea}</p>
+        <p><b class="fw-bolder">Category :</b> ${meal.strCategory}</p>
+        <h3>Recipes :</h3>
+        <ul class="d-flex " id="recipes">
+        </ul>
+
+        <h3 class="my-2 mx-1 p-1">Tags :</h3>
+        <ul class="d-flex " id="tags">
+        </ul>
+
+
+        <a class="btn btn-success text-white" target="_blank" href="${meal.strSource}">Source</a>
+        <a class="btn youtube text-white" target="_blank" href="${meal.strYoutube}">Youtub</a>
+    </div>`;
+
+  body.innerHTML = html;
+}
